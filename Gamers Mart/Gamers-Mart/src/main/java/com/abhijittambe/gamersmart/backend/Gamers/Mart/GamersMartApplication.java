eb351f3dd5a8f55534361900d267b384fe8f4fa1
Abhijit Tambe <abhijit.tambe@hotmail.com>
2020-04-13 21:04:00 -0400

    backend created

---
+++ Gamers Mart/Gamers-Mart/src/main/java/com/abhijittambe/gamersmart/backend/Gamers/Mart/GamersMartApplication.java
@@ -0,0 +1,13 @@
+package com.abhijittambe.gamersmart.backend.Gamers.Mart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamersMartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamersMartApplication.class, args);
+	}
+
+}
---
+++ Gamers Mart/Gamers-Mart/src/test/java/com/abhijittambe/gamersmart/backend/Gamers/Mart/GamersMartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhijittambe.gamersmart.backend.Gamers.Mart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamersMartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
