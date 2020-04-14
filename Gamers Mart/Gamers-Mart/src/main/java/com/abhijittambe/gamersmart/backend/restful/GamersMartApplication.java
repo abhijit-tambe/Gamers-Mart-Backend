4851ae31d1d7da7edb1e722f4108c67ef84057c6
Abhijit Tambe <abhijit.tambe@hotmail.com>
2020-04-13 21:41:47 -0400

    Update GamersMartApplication.java

--- Gamers Mart/Gamers-Mart/src/main/java/com/abhijittambe/gamersmart/backend/restful/GamersMartApplication.java
+++ Gamers Mart/Gamers-Mart/src/main/java/com/abhijittambe/gamersmart/backend/restful/GamersMartApplication.java
@@ -1,4 +1,4 @@
-package com.abhijittambe.gamersmart.backend.Gamers.Mart;
+package com.abhijittambe.gamersmart.backend.restful;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +8,7 @@ public class GamersMartApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GamersMartApplication.class, args);
+		System.out.print("hello");
 	}
 
 }
