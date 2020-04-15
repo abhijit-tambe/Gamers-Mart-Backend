635b1b10f173190d29b5827ab0a818b0f5a09f9e
Abhijit Tambe <abhijit.tambe@hotmail.com>
2020-04-14 22:13:20 -0400

    Update UsersResourceController.java

--- Gamers Mart/Gamers-Mart/src/main/java/com/abhijittambe/gamersmart/backend/restful/webservices/users/UsersResourceController.java
+++ Gamers Mart/Gamers-Mart/src/main/java/com/abhijittambe/gamersmart/backend/restful/webservices/users/UsersResourceController.java
@@ -3,10 +3,12 @@ package com.abhijittambe.gamersmart.backend.restful.webservices.users;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins="http://localhost:3000")
 public class UsersResourceController {
 	
 	@Autowired
