78c4da5264242567076521dbf322c8ae4921bb7a
Abhijit Tambe <abhijit.tambe@hotmail.com>
2020-04-16 18:45:51 -0400

    Update data.sql

--- Gamers Mart/Gamers-Mart/src/main/resources/data.sql
+++ Gamers Mart/Gamers-Mart/src/main/resources/data.sql
@@ -4,3 +4,4 @@ insert into Items(item_id,user_name,game_name,item_type,item_name,description,pr
 insert into Items(item_id,user_name,game_name,item_type,item_name,description,price) values(51,'abhijit2','CS Go','Collectible','ak 47','bla bla bla',4.9);
 insert into Items(item_id,user_name,game_name,item_type,item_name,description,price) values(52,'abhijit2','CS Go','Collectible','ak 47','bla bla bla',4.9);
 insert into Items(item_id,user_name,game_name,item_type,item_name,description,price) values(53,'abhijit','Dota 2','Collectible','faceless void','bla bla bla',45.5);
+insert into Items(item_id,user_name,game_name,item_type,item_name,description,price) values(54,'abhijit','Portal 2','Cd Key','Portal 2','bla bla bla',60);
