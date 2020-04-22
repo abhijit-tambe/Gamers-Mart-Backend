888c1710da3c82b3318212835fb46ea4a4d03296
Abhijit Tambe <abhijit.tambe@hotmail.com>
2020-04-22 16:18:48 -0400

    Update UsersResourceController.java

--- Gamers Mart/Gamers-Mart/src/main/java/com/abhijittambe/gamersmart/backend/restful/webservices/users/UsersResourceController.java
+++ Gamers Mart/Gamers-Mart/src/main/java/com/abhijittambe/gamersmart/backend/restful/webservices/users/UsersResourceController.java
@@ -3,14 +3,20 @@ package com.abhijittambe.gamersmart.backend.restful.webservices.users;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @CrossOrigin(origins="http://localhost:3000")
 public class UsersResourceController {
 	
+	RestTemplate restTemplate = new RestTemplate();
 	@Autowired
 	UsersHardService uHS;
 	
@@ -18,6 +24,12 @@ public class UsersResourceController {
 	@GetMapping(path="/users")
 	public List<Users> GetAllUsers() {
 		return uHS.findAll();
+	}
+	
+	@PostMapping(path="/create-account")
+	public ResponseEntity<Void> createUser( @RequestBody Users user){
+		uHS.save(user);
+		return ResponseEntity.noContent().build();
 	}
 	
 }
