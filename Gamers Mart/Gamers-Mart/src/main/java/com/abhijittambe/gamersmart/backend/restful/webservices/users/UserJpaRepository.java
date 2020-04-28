1cea3a906e322206e5d729b5c088d9df2f3a91c7
Abhijit Tambe <abhijit.tambe@hotmail.com>
2020-04-28 18:58:49 -0400

    Create UserJpaRepository.java

---
+++ Gamers Mart/Gamers-Mart/src/main/java/com/abhijittambe/gamersmart/backend/restful/webservices/users/UserJpaRepository.java
@@ -0,0 +1,7 @@
+package com.abhijittambe.gamersmart.backend.restful.webservices.users;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserJpaRepository extends JpaRepository<Users, Long>{
+
+}
