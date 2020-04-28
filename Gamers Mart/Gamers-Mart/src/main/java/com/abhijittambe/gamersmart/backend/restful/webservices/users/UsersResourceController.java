1b5f5114c6c855c45c6b8c5982984d13e5f9b6c6
Abhijit Tambe <abhijit.tambe@hotmail.com>
2020-04-28 19:11:33 -0400

    Update UsersResourceController.java

--- Gamers Mart/Gamers-Mart/src/main/java/com/abhijittambe/gamersmart/backend/restful/webservices/users/UsersResourceController.java
+++ Gamers Mart/Gamers-Mart/src/main/java/com/abhijittambe/gamersmart/backend/restful/webservices/users/UsersResourceController.java
@@ -12,37 +12,47 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.abhijittambe.gamersmart.backend.restful.jwt.JwtInMemoryUserDetailsService;
+
 
 
 @RestController
 @CrossOrigin(origins="http://localhost:3000")
 public class UsersResourceController {
-	
-//	RestTemplate restTemplate = new RestTemplate();
+
 	@Autowired
-	UsersHardService uHS;
+	UserJpaRepository userJpaRepository;
+	@Autowired
+	JwtInMemoryUserDetailsService jwtInMemory;
 	
+	@GetMapping(path="/")
+	public ResponseEntity<Void> initialise() {
+		List<Users> users= userJpaRepository.findAll();
+			for(Users u:users) {
+				jwtInMemory.addUser(u.getUserId(),u.getUserName(),u.getPassword());
+			}
+			System.out.println("initialization complete");
+		return new ResponseEntity<Void>(HttpStatus.OK);
+	}
 	
 	@GetMapping(path="/user/allusers")
 	public List<Users> GetAllUsers() {
-		return uHS.findAll();
+		return userJpaRepository.findAll();
 	}
 	
 	@PostMapping(path="/user/create-account")
 	public ResponseEntity<Void> createUser( @RequestBody Users user){
-		
-		
-		Users updateuser= uHS.save(user);
-		System.out.print(updateuser.getUserId());
-//		System.out.println("user email"+user.getUserId());
-//		if(user.getUserId()!=0||user.getUserId()!=-1)
+		Users userCreated= userJpaRepository.save(user);
+		jwtInMemory.addUser(userCreated.getUserId(),userCreated.getUserName(),userCreated.getPassword());
+		if(userCreated.getUserId()!=0||user.getUserId()!=-1)
 		return new ResponseEntity<Void>(HttpStatus.OK);
-//		else
-//		return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
+		else
+		return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
 	}
 	
 	@PostMapping(path="/user/acs")
 	public String dasdas(){
 		return "works";
 	}
+	
 }
