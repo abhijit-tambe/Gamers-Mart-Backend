d4a6952ada85d21da0d7bf6e41c8d3926a27fdf2
Abhijit Tambe <abhijit.tambe@hotmail.com>
2020-04-28 18:59:01 -0400

    Update Users.java

--- Gamers Mart/Gamers-Mart/src/main/java/com/abhijittambe/gamersmart/backend/restful/webservices/users/Users.java
+++ Gamers Mart/Gamers-Mart/src/main/java/com/abhijittambe/gamersmart/backend/restful/webservices/users/Users.java
@@ -1,10 +1,17 @@
 package com.abhijittambe.gamersmart.backend.restful.webservices.users;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 
-
+@Entity
 public class Users {
 
-	
+	@Id
+	@SequenceGenerator(name="mysequence", initialValue=5)
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="mysequence")
 	private Long userId;
 	private String emailId;
 	private String userName;
@@ -13,6 +20,7 @@ public class Users {
 	
 	public Users() {
 		super();
+		
 		// TODO Auto-generated constructor stub
 	}
 	public Users(Long userId, String emailId, String userName, String password, String phoneNumber) {
@@ -55,6 +63,55 @@ public class Users {
 	}
 	public void setPhoneNumber(String phoneNumber) {
 		this.phoneNumber = phoneNumber;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((emailId == null) ? 0 : emailId.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((phoneNumber == null) ? 0 : phoneNumber.hashCode());
+		result = prime * result + ((userId == null) ? 0 : userId.hashCode());
+		result = prime * result + ((userName == null) ? 0 : userName.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Users other = (Users) obj;
+		if (emailId == null) {
+			if (other.emailId != null)
+				return false;
+		} else if (!emailId.equals(other.emailId))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (phoneNumber == null) {
+			if (other.phoneNumber != null)
+				return false;
+		} else if (!phoneNumber.equals(other.phoneNumber))
+			return false;
+		if (userId == null) {
+			if (other.userId != null)
+				return false;
+		} else if (!userId.equals(other.userId))
+			return false;
+		if (userName == null) {
+			if (other.userName != null)
+				return false;
+		} else if (!userName.equals(other.userName))
+			return false;
+		return true;
 	}
 	
 	
