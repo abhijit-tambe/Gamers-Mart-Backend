f2cd4fb58fcceeab6ca7f2fe5f3dddd11bc49b58
Abhijit Tambe <abhijit.tambe@hotmail.com>
2020-04-28 18:51:06 -0400

    Update data.sql

--- Gamers Mart/Gamers-Mart/src/main/resources/data.sql
+++ Gamers Mart/Gamers-Mart/src/main/resources/data.sql
@@ -5,3 +5,10 @@ insert into Items(item_id,user_name,game_name,item_type,item_name,description,pr
 insert into Items(item_id,user_name,game_name,item_type,item_name,description,price) values(52,'abhijit2','CS Go','Collectible','ak 47','bla bla bla',4.9);
 insert into Items(item_id,user_name,game_name,item_type,item_name,description,price) values(53,'abhijit3','Dota 2','Collectible','faceless void','bla bla bla',45.5);
 insert into Items(item_id,user_name,game_name,item_type,item_name,description,price) values(54,'abhijit','Portal 2','Cd Key','Portal 2','bla bla bla',60);
+
+insert into Users(user_id,email_id,user_name,password,phone_number) values (40,'abc@gmail.com','abhijit','tambe','4513265623');
+insert into Users(user_id,email_id,user_name,password,phone_number) values (41,'abc@gmail.com','abhijit2','tambe2','4513265623');
+insert into Users(user_id,email_id,user_name,password,phone_number) values (42,'abc@gmail.com','abhijit3','tambe3','4513265623');
+insert into Users(user_id,email_id,user_name,password,phone_number) values (43,'abc@gmail.com','abhijit4','tambe4','4513265623');
+
+
