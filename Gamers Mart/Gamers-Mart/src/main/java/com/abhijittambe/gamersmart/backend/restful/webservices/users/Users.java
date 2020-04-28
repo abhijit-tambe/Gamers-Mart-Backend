1e7693d5a9188d846bd6eb7f9138bbf1527ef537
Abhijit Tambe <abhijit.tambe@hotmail.com>
2020-04-27 20:03:33 -0400

    Update Users.java

--- Gamers Mart/Gamers-Mart/src/main/java/com/abhijittambe/gamersmart/backend/restful/webservices/users/Users.java
+++ Gamers Mart/Gamers-Mart/src/main/java/com/abhijittambe/gamersmart/backend/restful/webservices/users/Users.java
@@ -5,7 +5,7 @@ package com.abhijittambe.gamersmart.backend.restful.webservices.users;
 public class Users {
 
 	
-	private int userId;
+	private Long userId;
 	private String emailId;
 	private String userName;
 	private String password;
@@ -15,7 +15,7 @@ public class Users {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public Users(int userId, String emailId, String userName, String password, String phoneNumber) {
+	public Users(Long userId, String emailId, String userName, String password, String phoneNumber) {
 		super();
 		this.userId = userId;
 		this.emailId = emailId;
@@ -26,10 +26,10 @@ public class Users {
 	
 	
 	
-	public int getUserId() {
+	public Long getUserId() {
 		return userId;
 	}
-	public void setUserId(int userId) {
+	public void setUserId(Long userId) {
 		this.userId = userId;
 	}
 	public String getEmailId() {
