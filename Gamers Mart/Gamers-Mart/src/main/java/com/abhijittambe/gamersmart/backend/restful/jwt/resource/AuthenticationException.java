93293e30cf0e5cbf19b8efddcb137bba63604667
Abhijit Tambe <abhijit.tambe@hotmail.com>
2020-04-27 20:03:46 -0400

    Create AuthenticationException.java

---
+++ Gamers Mart/Gamers-Mart/src/main/java/com/abhijittambe/gamersmart/backend/restful/jwt/resource/AuthenticationException.java
@@ -0,0 +1,7 @@
+package com.abhijittambe.gamersmart.backend.restful.jwt.resource;
+public class AuthenticationException extends RuntimeException {
+    public AuthenticationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
