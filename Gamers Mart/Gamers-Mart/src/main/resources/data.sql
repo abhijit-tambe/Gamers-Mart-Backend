ea23b135a38c0ef3c1986ac7c9410acec2fd18a9
Abhijit Tambe <abhijit.tambe@hotmail.com>
2020-04-29 17:06:42 -0400

    Update data.sql

--- Gamers Mart/Gamers-Mart/src/main/resources/data.sql
+++ Gamers Mart/Gamers-Mart/src/main/resources/data.sql
@@ -1,14 +1,14 @@
 
-insert into Items(item_id,user_name,game_name,item_type,item_name,description,price) values(1,'abhijit','Dota 2','Collectible','axe cleaver','bla bla bla',45.5);
-insert into Items(item_id,user_name,game_name,item_type,item_name,description,price) values(2,'abhijit','CS Go','Collectible','antimage','bla bla bla',45.9);
-insert into Items(item_id,user_name,game_name,item_type,item_name,description,price) values(3,'abhijit1','CS Go','Collectible','ak 47','bla bla bla',4.9);
-insert into Items(item_id,user_name,game_name,item_type,item_name,description,price) values(4,'abhijit2','CS Go','Collectible','ak 47','bla bla bla',4.9);
-insert into Items(item_id,user_name,game_name,item_type,item_name,description,price) values(5,'abhijit3','Dota 2','Collectible','faceless void','bla bla bla',45.5);
-insert into Items(item_id,user_name,game_name,item_type,item_name,description,price) values(6,'abhijit','Portal 2','Cd Key','Portal 2','bla bla bla',60);
+insert into Item(item_id,user_name,game_name,item_type,item_name,description,price) values(1,'abhijit','Dota 2','Collectible','axe cleaver','bla bla bla',45.5);
+insert into Item(item_id,user_name,game_name,item_type,item_name,description,price) values(2,'abhijit','CS Go','Collectible','antimage','bla bla bla',45.9);
+insert into Item(item_id,user_name,game_name,item_type,item_name,description,price) values(3,'abhijit1','CS Go','Collectible','ak 47','bla bla bla',4.9);
+insert into Item(item_id,user_name,game_name,item_type,item_name,description,price) values(4,'abhijit2','CS Go','Collectible','ak 47','bla bla bla',4.9);
+insert into Item(item_id,user_name,game_name,item_type,item_name,description,price) values(5,'abhijit3','Dota 2','Collectible','faceless void','bla bla bla',45.5);
+insert into Item(item_id,user_name,game_name,item_type,item_name,description,price) values(6,'abhijit','Portal 2','Cd Key','Portal 2','bla bla bla',60);
 
-insert into Users(user_id,email_id,user_name,password,phone_number) values (1,'abc@gmail.com','abhijit','tambe','4513265623');
-insert into Users(user_id,email_id,user_name,password,phone_number) values (2,'abc@gmail.com','abhijit2','tambe2','4513265623');
-insert into Users(user_id,email_id,user_name,password,phone_number) values (3,'abc@gmail.com','abhijit3','tambe3','4513265623');
-insert into Users(user_id,email_id,user_name,password,phone_number) values (4,'abc@gmail.com','abhijit4','tambe4','4513265623');
+insert into User(user_id,email_id,user_name,password,phone_number) values (1,'abc@gmail.com','abhijit','tambe','4513265623');
+insert into User(user_id,email_id,user_name,password,phone_number) values (2,'abc@gmail.com','abhijit2','tambe2','4513265623');
+insert into User(user_id,email_id,user_name,password,phone_number) values (3,'abc@gmail.com','abhijit3','tambe3','4513265623');
+insert into User(user_id,email_id,user_name,password,phone_number) values (4,'abc@gmail.com','abhijit4','tambe4','4513265623');
 
 
