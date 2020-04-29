73cf78d7ea3f8aa4eb922ab56cda18fb3ea5a906
Abhijit Tambe <abhijit.tambe@hotmail.com>
2020-04-28 21:30:10 -0400

    Update ItemsResourceController.java

--- Gamers Mart/Gamers-Mart/src/main/java/com/abhijittambe/gamersmart/backend/restful/webservices/items/ItemsResourceController.java
+++ Gamers Mart/Gamers-Mart/src/main/java/com/abhijittambe/gamersmart/backend/restful/webservices/items/ItemsResourceController.java
@@ -3,9 +3,15 @@ package com.abhijittambe.gamersmart.backend.restful.webservices.items;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,10 +26,44 @@ public class ItemsResourceController {
 	public List<Items> getAllItemListings(){
 		return itemRepository.findAll();
 	}
+	
 
 	@GetMapping(path="/items/{username}/mylistings")
 	public List<Items> getUserItemListings(@PathVariable String username){
 		return itemRepository.findByUserName(username);
 	}
+	
+	@DeleteMapping(path="/items/{username}/delete/{itemid}")
+	public ResponseEntity<Void> deleteUserItem(@PathVariable String username, @PathVariable Long itemid){
+		Items deleteItem = itemRepository.getOne(itemid);
+		itemRepository.delete(deleteItem);
+		return new ResponseEntity<Void>(HttpStatus.OK);
+	}
+	
+	@PostMapping(path="/items/{username}/add")
+	public ResponseEntity<Void> addUserItem(@PathVariable String username,@RequestBody Items item){
+		item.setUserName(username);
+		itemRepository.save(item);
+		return new ResponseEntity<Void>(HttpStatus.OK);
+	}
+	
+	@PutMapping(path="/items/{username}/update/{itemid}")
+		public ResponseEntity<Void> updateUserItem(@PathVariable String username, @PathVariable Long itemid, @RequestBody Items item){
+			Items updateItem = itemRepository.getOne(itemid);
+			updateItem.setDescription(item.getDescription());
+			updateItem.setPrice(item.getPrice());
+			itemRepository.save(updateItem);
+			return new ResponseEntity<Void>(HttpStatus.OK);
+		}	
+	
+	@GetMapping(path="/items/{username}/buy/{itemid}")
+	public ResponseEntity<Void> buyItems(@PathVariable String username, @PathVariable Long itemid){
+		Items boughtItem = itemRepository.getOne(itemid);
+		boughtItem.setUserName(username);
+		itemRepository.save(boughtItem);
+		return new ResponseEntity<Void>(HttpStatus.OK);
+	}
+
+	
 	
 }
